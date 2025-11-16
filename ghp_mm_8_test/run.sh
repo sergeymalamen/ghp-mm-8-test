753553fc47a9778dca560f01ae43fe9249e264b6
sergeymalamen <sergey.malamen@gmail.com>
2025-11-16 21:13:24 +0100

    Start

---
+++ ghp_mm_8_test/run.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/with-contenv bashio
+
+. /venv/bin/activate
+
+bashio::log.info "✅ run.sh запущен как PID: $$"
+
+SERIAL_PORT=$(bashio::config 'serial_port')
+MQTT_BROKER=$(bashio::config 'mqtt_broker')
+MQTT_PORT=$(bashio::config 'mqtt_port')
+MQTT_USERNAME=$(bashio::config 'mqtt_username')
+MQTT_PASSWORD=$(bashio::config 'mqtt_password')
+
+bashio::log.info "🔧 Конфигурация:"
+bashio::log.info "Serial: ${SERIAL_PORT}"
+bashio::log.info "Broker: ${MQTT_BROKER}:${MQTT_PORT}"
+bashio::log.info "User: ${MQTT_USERNAME}"
+
+exec python3 /usr/src/app/ghp-mm2mqtt.py
